fe6499d03233d63643bea75e7d041f87aa2fa9d2
Andrey Demidov <andrey.demidov@gmail.com>
2011-05-16 10:17:09 +0400

    scheme : problem 1 : refactore

--- Problem_1/problem_1.rkt
+++ Problem_1/problem_1.rkt
@@ -2,16 +2,20 @@
 
 (define (accum? num)
   (or (= (modulo num 3) 0) (= (modulo num 5) 0)))
-  
-(define (helper i limit accum)
+             
+(define (problem1 limit)
+
+  (define (helper i limit accum)
   (cond 
     [(= i limit) accum]
     [else 
      (cond 
        [(accum? i) (helper (+ i 1) limit (+ accum i))]
        [else (helper (+ i 1) limit accum)])]))
-           
-(define (problem1 limit)
+  
+  (define (accum? num)
+    (or (= (modulo num 3) 0) (= (modulo num 5) 0)))
+
   (helper 0 limit 0))
 
 (problem1 1000)
