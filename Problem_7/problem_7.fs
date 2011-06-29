bd9652a9e9285fb14147d389534923e58ed9c30e
Andrey Demidov <andrey.demidov@gmail.com>
2011-06-29 13:08:06 +0400

    problem_7 : f#

---
+++ Problem_7/problem_7.fs
@@ -0,0 +1,23 @@
+open System
+
+let list_of_dividers num =
+    [2 .. int (floor (sqrt (float num))) + 1]
+    |> Seq.filter (fun x -> num % x = 0)
+
+let is_prime num =
+    (Seq.length (list_of_dividers num)) = 0
+
+let print_seq num =
+    for x in (list_of_dividers num) do
+        printfn "%d" x
+
+let rec problem_7 current primes counter =
+    match primes, counter with
+        | x, y when x >= 10002 -> current
+        | x, y when x< 10002 && (is_prime counter) -> problem_7 counter (primes + 1) (counter + 1)
+        | _, _ -> problem_7 current primes (counter + 1)
+
+[<EntryPoint>]
+let main(args : string[]) =
+    printfn "result = %d" (problem_7 0 0 0)
+    0
