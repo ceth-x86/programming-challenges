a81dfee2a17b4d13761d8b52ae3fb410dad0c755
Andrey Demidov <andrey.demidov@gmail.com>
2011-10-05 22:06:51 +0400

    programming praxis : 001 : ruby

---
+++ programming-praxis/001 - rpn calculator/calc.rb
@@ -0,0 +1,23 @@
+expression = "19 2.14 + 4.5 2 4.3 / - *"
+
+stack = Array.new
+
+expression.split.each { |element|
+  if ["+", "-", "*", "/"].include? element
+    a, b = stack.pop(2)
+    case element
+      when "+"
+      stack.push (a + b)
+    when "-"
+      stack.push (a - b)
+      when "*"
+      stack.push (a * b)
+      when "/"
+      stack.push (a / b).to_f
+    end
+  elsif
+    stack.push element.to_f
+  end
+}
+
+puts stack
