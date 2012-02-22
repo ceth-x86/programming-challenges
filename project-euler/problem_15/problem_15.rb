1d535139d5b180e1d363f98bc160b58ceeb5eb16
andrey demidov <andrey.demidov@gmail.com>
2012-02-23 00:11:25 +0400

    project euler : problem 15 : ruby : refactore

--- project-euler/problem_15/problem_15.rb
+++ project-euler/problem_15/problem_15.rb
@@ -1,39 +1,58 @@
-class Problem_15
+class Problem_15 < Hash
 	def initialize(size)
-		@grid_size = size
-		@cache = {}
+		@grid_size = size		
 	end
 
-	def calculate
-		find_solution(0, 0)
+	def calculate		
+		find_solution(ElementOfSolution.new(0, 0, @grid_size))		
 	end
 
-	def area(x, y)
-		(@grid_size - x) * (@grid_size - y)
-	end
-
-	def find_solution(x, y)
-		cur_area = area(x, y)
-		if(cur_area == 0)
+	def find_solution(element)		
+		if(element.area == 0)
 			return 1
 		end				
 
 		i = 0
-		key = (@grid_size - x).to_s + "x" + (@grid_size - y).to_s			
 
-		if(not @cache.has_key?(key))			
-			if(x < @grid_size)
-				i += find_solution(x + 1, y)
+		if(not self.has_key?(element.key))			
+			if(element.can_move_right)
+				i += find_solution(element.move_right)
 			end
 
-			if(y < @grid_size)
-				i += find_solution(x, y + 1)
+			if(element.can_move_down)
+				i += find_solution(element.move_down)
 			end
 			
-			@cache[key] = i
+			self[element.key] = i
 		end
 
-		return @cache[key]
+		return self[element.key]
+	end
+end
+
+class ElementOfSolution
+	attr_reader :key, :area
+
+	def initialize(x, y, size)
+		@x, @y, @size = x, y, size
+		@key = (@size - @x).to_s + "x" + (@size - @y).to_s
+		@area = (@size - @x) * (@size - @y)
+	end
+
+	def can_move_right
+		@x < @size
+	end
+
+	def can_move_down
+		@y < @size
+	end
+
+	def move_right
+		ElementOfSolution.new(@x + 1, @y, @size)
+	end
+
+	def move_down
+		ElementOfSolution.new(@x, @y + 1, @size)
 	end
 end
 
