429cf957e6122cc2b566d6470c3f05652f1511e1
Andrey Demidov <andrey.demidov@gmail.com>
2012-09-27 16:41:28 +0400

    project euler : problem_01 : go lang

---
+++ project-euler/Problem_01/problem_1.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	result := 0
+
+	for i := 1; i <= 1000; i++ {
+		if ((i % 3 == 0) || (i % 5 == 0)) {
+			result += i
+		}
+	}	
+
+	fmt.Println("Result :", result)
+}
